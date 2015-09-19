7d365a25b0a3d75e05a01294b8ccb7d4af4811fc
Fabio Napoleoni <f.napoleoni@gmail.com>
2015-09-19 19:53:01 +0200

    Working implementation as interceptor, filter still missing.

--- lib/google_analytics_mailer/injector.rb
+++ lib/google_analytics_mailer/injector.rb
@@ -5,11 +5,39 @@ class GoogleAnalyticsMailer::Injector
 
   def initialize(email, params)
     @email = email
-    @params = params.dup.freeze
+    @params = params
+    @builder = GoogleAnalyticsMailer::UriBuilder.new
   end
 
   def process!
-    # TODO complete
+    # Replace links in all html parts of a message
+    process_html_part do |doc|
+      rewrite!(doc) { |a| @builder.build(a[:href], params.dup) }
+    end
+  end
+
+  private
+
+  # Yield a nokogiri document with the message html part and replace it with
+  # the processed document.
+  #
+  # @yieldparam [Nokogiri::HTML::Document] nokogiri document that represents the html part
+  def process_html_part(&block)
+    body = email.multipart? ? email.html_part.body : email.body
+    document = Nokogiri::HTML(body.raw_source)
+    block.call(document)
+    # Replace the html part with the processed document
+    body.raw_source.replace(document.to_s)
+  end
+
+  # Update link content in nokogiri document
+  # @param [Nokogiri::HTML::Document] document html document to update
+  # @yield A block used to perform url transformations
+  # @yieldparam [Nokogiri::XML::Element] nokogiri node representing a link
+  def rewrite!(document, &block)
+    document.css('a[href]').each do |link|
+      link[:href] = block.call(link)
+    end
   end
 
 end
