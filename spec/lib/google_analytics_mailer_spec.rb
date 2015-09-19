f93dde403f673ca86985f24e78a9e45c183b3d6c
Fabio Napoleoni <f.napoleoni@gmail.com>
2015-09-19 15:47:46 +0200

    Convert specs to RSpec 3.3.2 syntax with Transpec
    This conversion is done by Transpec 3.1.1 with the following command:
        transpec
    
    * 15 conversions
        from: obj.should
          to: expect(obj).to
    
    * 3 conversions
        from: == expected
          to: eq(expected)
    
    * 2 conversions
        from: obj.should_not
          to: expect(obj).not_to
    
    * 1 addition
          of: RSpec.configure { |c| c.infer_spec_type_from_file_location! }
    
    For more details: https://github.com/yujinakayama/transpec#supported-conversions

--- spec/lib/google_analytics_mailer_spec.rb
+++ spec/lib/google_analytics_mailer_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe GoogleAnalyticsMailer do
 
   it 'ActionMailer::Base should extend GoogleAnalyticsMailer' do
-    (class << ActionMailer::Base; self end).included_modules.should include(GoogleAnalyticsMailer)
+    expect((class << ActionMailer::Base; self end).included_modules).to include(GoogleAnalyticsMailer)
   end
 
   describe '.google_analytics_mailer' do
@@ -20,11 +20,11 @@ describe GoogleAnalyticsMailer do
     it 'should assign given parameters to a class variable' do
       params = {utm_source: 'newsletter', utm_medium: 'email'}
       TestMailer1.google_analytics_mailer(params)
-      TestMailer1.google_analytics_class_params.should == params
+      expect(TestMailer1.google_analytics_class_params).to eq(params)
     end
 
     it 'should be aliased to google_analytics_controller' do
-      ActionMailer::Base.should respond_to :google_analytics_controller
+      expect(ActionMailer::Base).to respond_to :google_analytics_controller
     end
 
   end
@@ -74,15 +74,15 @@ describe GoogleAnalyticsMailer do
       subject { FilteredMailer.welcome }
 
       it 'should have analytics link with params taken from class definition' do
-        subject.should have_body_text 'http://www.example.com/newsletter?utm_medium=email&utm_source=newsletter'
+        expect(subject).to have_body_text 'http://www.example.com/newsletter?utm_medium=email&utm_source=newsletter'
       end
 
       it 'should have analytics link with overridden params' do
-        subject.should have_body_text 'http://www.example.com/newsletter?utm_medium=email&utm_source=my_newsletter'
+        expect(subject).to have_body_text 'http://www.example.com/newsletter?utm_medium=email&utm_source=my_newsletter'
       end
 
       it 'should have non-http link with no tracking' do
-        subject.should have_body_text '"http://www.external.com/"'
+        expect(subject).to have_body_text '"http://www.external.com/"'
       end
     end
   end
@@ -95,11 +95,11 @@ describe GoogleAnalyticsMailer do
       subject { UserMailer.welcome }
 
       it 'should have analytics link with params taken from class definition' do
-        subject.should have_body_text 'http://www.example.com/newsletter?utm_medium=email&utm_source=newsletter'
+        expect(subject).to have_body_text 'http://www.example.com/newsletter?utm_medium=email&utm_source=newsletter'
       end
 
       it 'should have analytics link with overridden params' do
-        subject.should have_body_text 'http://www.example.com/newsletter?utm_medium=email&utm_source=my_newsletter'
+        expect(subject).to have_body_text 'http://www.example.com/newsletter?utm_medium=email&utm_source=my_newsletter'
       end
 
     end
@@ -110,11 +110,11 @@ describe GoogleAnalyticsMailer do
       subject { UserMailer.welcome2 }
 
       it 'should have analytics link with params taken from instance' do
-        subject.should have_body_text 'http://www.example.com/newsletter?utm_medium=email&utm_source=second_newsletter&utm_term=welcome2'
+        expect(subject).to have_body_text 'http://www.example.com/newsletter?utm_medium=email&utm_source=second_newsletter&utm_term=welcome2'
       end
 
       it 'should have analytics link with overridden params' do
-        subject.should have_body_text 'http://www.example.com/newsletter?utm_medium=email&utm_source=my_newsletter&utm_term=welcome2'
+        expect(subject).to have_body_text 'http://www.example.com/newsletter?utm_medium=email&utm_source=my_newsletter&utm_term=welcome2'
       end
 
     end
@@ -125,7 +125,7 @@ describe GoogleAnalyticsMailer do
       subject { UserMailer.welcome3 }
 
       it 'should have analytics link with params taken from view' do
-        subject.should have_body_text 'http://www.example.com/newsletter?utm_medium=email&utm_source=newsletter&utm_term=footer'
+        expect(subject).to have_body_text 'http://www.example.com/newsletter?utm_medium=email&utm_source=newsletter&utm_term=footer'
       end
 
     end
--- spec/lib/uri_builder_spec.rb
+++ spec/lib/uri_builder_spec.rb
@@ -5,30 +5,30 @@ describe GoogleAnalyticsMailer::UriBuilder do
   describe '#build' do
 
     it 'should not include blank GA params' do
-      subject.build('http://www.example.com', utm_campaign: nil).should_not include 'utm_campaign'
+      expect(subject.build('http://www.example.com', utm_campaign: nil)).not_to include 'utm_campaign'
     end
 
     it 'should uri encode GA parameter values' do
-      subject.build('http://www.example.com', utm_campaign: 'Foo Bar').should include 'utm_campaign=Foo%20Bar'
+      expect(subject.build('http://www.example.com', utm_campaign: 'Foo Bar')).to include 'utm_campaign=Foo%20Bar'
     end
 
     it 'should not add GA parameters if uri is not absolute' do
-      subject.build('/some/path', utm_campaign: 'abc').should == '/some/path'
+      expect(subject.build('/some/path', utm_campaign: 'abc')).to eq('/some/path')
     end
 
     it 'should not touch uri with empty params' do
-      subject.build('http://www.example.com', {}).should == 'http://www.example.com'
+      expect(subject.build('http://www.example.com', {})).to eq('http://www.example.com')
     end
 
     context 'with a filter' do
       subject { GoogleAnalyticsMailer::UriBuilder.new ->(uri) { uri.host == 'www.example.com' } }
 
       it 'should add GA parameters to URIs that match the filter' do
-        subject.build('http://www.example.com', utm_campaign: 'Foo Bar').should include 'utm_campaign=Foo%20Bar'
+        expect(subject.build('http://www.example.com', utm_campaign: 'Foo Bar')).to include 'utm_campaign=Foo%20Bar'
       end
 
       it 'should not add GA parameters to URIs that do not match the filter' do
-        subject.build('http://www.external.com', utm_campaign: 'Foo Bar').should_not include 'utm_campaign=Foo%20Bar'
+        expect(subject.build('http://www.external.com', utm_campaign: 'Foo Bar')).not_to include 'utm_campaign=Foo%20Bar'
       end
     end
 
